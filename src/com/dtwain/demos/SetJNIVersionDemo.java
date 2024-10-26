771c68409a36ab4b9fbbc29db990d65f8d018849
Dynarithmic <paulm@dynarithmic.com>
2024-10-26 17:21:18 -0400

    Minor change in comment

--- src/com/dtwain/demos/SetJNIVersionDemo.java
+++ src/com/dtwain/demos/SetJNIVersionDemo.java
@@ -30,6 +30,7 @@ import com.dynarithmic.twain.highlevel.TwainSource;
 
 public class SetJNIVersionDemo
 {
+    // Simple acquire to a file
     public void run() throws Exception
     {
         System.out.println("Please choose the JNI to use for your application:\n");
